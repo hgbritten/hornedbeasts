12227188825825e21a0b3db0fd7738d20ec86dde
hunter britten <huntergbritten@gmail.com>
2021-04-13 14:30:11 -0700

    cards!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,73 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Main from './components/main.js';
-
+import { Container, CardColumns, Card, Navbar } from 'react-bootstrap';
+// import Main from './components/main.js';
+import { Component } from 'react';
+import items from './data.json';
+//inspired by likeablecard demo in class
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <Container fluid>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="#">Horned Beasts</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <CardColumns>
+
+        {items.map(item => (
+          <LikeableBeast
+            title={item.title}
+            description={item.description}
+            imgUrl={item.image_url}
+          />
+        ))}
+
+      </CardColumns>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="#">Pick your favorites!</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
   );
 }
 
-function Header() {
-  return (
-    <h1>
-      Horned Beasts
-    </h1>
-  );
-}
+class LikeableBeast extends Component {
 
-function Footer() {
-  return (
-    <h1>
-      By: Hunter Britten
-    </h1>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+    };
+  }
+
+  addFavorite = () => {
+    this.setState({ likes: this.state.likes + 1 });
+  }
+
+  render() {
+    return (
+      <Card
+        bg="light"
+        text="dark"
+        onClick={this.addFavorite}
+      >
+        <Card.Img src={this.props.imgUrl} variant="top" />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>
+            🤘 = {this.state.likes}
+
+          </Card.Text>
+          <Card.Text>
+            {this.props.description}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default App;
